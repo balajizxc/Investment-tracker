4aed04a12eddc3ee75be7467e98949758778c4d9
balajizxc <41898542+balajizxc@users.noreply.github.com>
2025-07-04 21:29:48 +0530

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { supabase } from "@/lib/supabase";
 
 export default function Dashboard() {
   const [user, setUser] = useState<any>(null);
